4ac718421bbe570afdbc13a33a454ada0d1998ee
Kevin.Twu <kevin.twu@appier.com>
2019-05-14 14:02:30 +0800

    upgrade discord.py version to 1.1.1

--- src/main.py
+++ src/main.py
@@ -31,13 +31,13 @@ async def on_message(message):
         return
 
     # increase exp for public chat
-    if not message.channel.is_private:
+    if not message.channel.__class__.__name__ == "DMChannel":
         Users.increase_value(message.author, "exp", len(message.content))
         Users.check_level_up(message.author)
 
     # response with user information
-    if message.channel.is_private:
-        # avoid additional actions if user is already busy 
+    if message.channel.__class__.__name__ == "DMChannel":
+        # avoid additional actions if user is already busy
         if message.author in busy_users:
             return
 
@@ -46,24 +46,16 @@ async def on_message(message):
         if message.content == "點數":
             user_info = Users.get(message.author)
             response = "你目前的點數為：{}".format(user_info["points"])
-            await client.send_message(message.channel, response)
-
-        elif message.content == "創團":
-            await Games.discord_create(client, message)
+            await message.channel.send(response)
 
         elif message.content == "開團":
-            await Games.discord_start(client, message)
-
-        elif message.content == "報名":
-            await Games.discord_join(client, message)
+            await games.create(client, message)
 
         else:
             response = "請輸入對應的指令：\n"
             response += "`點數`：查看你的點數\n"
-            response += "`創團`：登記團務，募集玩家\n"
-            response += "`開團`：收取點數，開始跑團\n"
-            response += "`報名`：參加募集中的團\n"
-            await client.send_message(message.channel, response)
+            response += "`開團`：登記團務，募集玩家\n"
+            await message.channel.send(response)
 
         busy_users.remove(message.author)
 
