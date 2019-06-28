424642b0591c0c58c0afbceb6e0fd4af646115c0
DolaTRPG <dolatrpg@gmail.com>
2019-06-28 18:29:27 +0800

    remove unused functions

--- src/main.py
+++ src/main.py
@@ -20,7 +20,6 @@ bot = commands.Bot(
     self_bot=False,
     owner_id=559563649841233951
 )
-busy_users = []
 
 bot.add_cog(users.Users(bot, google_spreadsheet_key, game_server_id, newcomer_role_name))
 bot.add_cog(dice.Dice(bot))
@@ -42,46 +41,12 @@ async def on_raw_reaction_add(event):
         await games.start(client, message, Users)
 
 
+"""
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    # reaction in direct message
-    if is_channel_type(message.channel, "DMChannel"):
-        # avoid additional actions if user is already busy
-        if message.author in busy_users:
-            return
-
-        busy_users.append(message.author)
-
-        elif message.content == "開團":
-            await games.create(client, message, game_channel_id)
-
-        # save current progress into storage
-        elif message.content.startswith('!save'):
-            await Users.write()
-            await message.channel.send("更新完成")
-
-        else:
-            response = "請輸入對應的指令：\n"
-            response += "`點數`：查看你的點數\n"
-            response += "`開團`：登記團務，募集玩家\n"
-            await message.channel.send(response)
-
-        busy_users.remove(message.author)
-
-
-def is_channel_type(channel, class_name):
-    """check if channel is private
-    Args:
-        - (discord channel) channel class
-        - (str) class_name - discord class name (e.g. DMChannel)
-    """
-    if channel.__class__.__name__ == class_name:
-        return True
-    return False
+    if message.content == "開團":
+        await games.create(client, message, game_channel_id)
+"""
 
 
 bot.run(token)
