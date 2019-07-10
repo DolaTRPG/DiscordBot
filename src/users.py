53034b80ec264c9c6370904c5a77911cf171fef5
DolaTRPG <dolatrpg@gmail.com>
2019-07-10 11:11:57 +0800

    remove user db in googlesheet

--- src/users.py
+++ src/users.py
@@ -16,9 +16,6 @@ class Users(commands.Cog, name="點數功能"):
         self.bot = bot
         self._server_id = int(configurations.key['server'])
         self._newcomer_role_name = configurations.key['newcomer_role']
-        self._columns = ["id", "name", "points", "gm", "points_earned", "player", "points_used", "last_activity"]
-        self._storage = googlesheet.Storage(google_spreadsheet_key, "users", self._columns)
-        self.read()
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
@@ -80,97 +77,6 @@ class Users(commands.Cog, name="點數功能"):
         comment = " ".join(args[2:])
         if comment:
             await target_discord_user.send("轉讓理由：{}".format(comment))
-        await self.write()
-
-    def read(self):
-        """load users from DB
-        Return:
-            (None)
-        """
-        data_rows = self._storage.read_data()
-        self._users = []
-        for row in data_rows:
-            user = {}
-            for key, value in zip(self._columns, row):
-                try:
-                    user[key] = int(value)
-                except ValueError:
-                    user[key] = value
-            self._users.append(user)
-
-    async def write(self):
-        """output users into DB
-        Return:
-            (str) output worksheet name (e.g. users_20190101_123456)
-        """
-        await self.ban_abandoned_users()
-        worksheet_name = self._storage.write_data(self._users)
-        return worksheet_name
-
-    async def add(self, discord_user):
-        """add new user
-        Args:
-            (Discord.User) discord user class
-        Return:
-            (dict) user information
-                {
-                    "id": 0,
-                    "name": "username",
-                    "points": 10,
-                    "gm": 10,
-                    "player": 3,
-                    "points_used": 100,
-                    "points_earned": 100,
-                    "exp": 0
-                }
-        """
-        if discord_user.id == self.bot.user.id:
-            # avoid adding bot into database
-            return
-
-        user = {}
-        for key in self._columns:
-            user[key] = 0
-        user["id"] = int(discord_user.id)
-        user["name"] = discord_user.name
-        user["last_activity"] = time.strftime('%Y-%m-%d %H:%M:%S')
-        self._users.append(user)
-        await self.write()
-        return user
-
-    def get(self, discord_user):
-        """get user information
-        Args:
-            (Discord.User) discord user class
-        Return:
-            (None) user not existed
-            (dict) user information
-                {
-                    "id": 0,
-                    "points": 10,
-                    "gm": 10,
-                    "player": 3,
-                    "points_used": 100,
-                    "points_earned": 100,
-                    "exp": 0
-                }
-        """
-        for user in self._users:
-            if user['id'] == int(discord_user.id):
-                return user
-        return None
-
-    def get_abandoned_user_ids(self):
-        """users which does not appear anymore
-        Return:
-            - (list) user ids
-        """
-        abandoned_users = []
-        for user in self._users:
-            user_activity = datetime.strptime(user['last_activity'], '%Y-%m-%d %H:%M:%S')
-            if user_activity < datetime.now() - timedelta(days=10):  # user inactive for 10 days will be listed as abandoned
-                abandoned_users.append(user['id'])
-        return abandoned_users
 
     async def ban_abandoned_users(self):
         """remove abandoned users from database and send announcement message
