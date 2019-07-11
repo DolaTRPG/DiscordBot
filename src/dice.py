0fe3913ac43fcbc79ec792aa628477b635a10b2a
DolaTRPG <dolatrpg@gmail.com>
2019-07-11 14:47:10 +0800

    新增功能：coc 擲骰判定

--- src/dice.py
+++ src/dice.py
@@ -21,10 +21,36 @@ class Dice(commands.Cog, name="骰子功能"):
             (total, rolls, modifier) = self._roll(dice_string)
             response = "{}\nroll {}".format(ctx.author.mention, dice_string)
             if dice_comment:
-                response += " ({})".format(dice_comment)
+                response += "\n{}".format(dice_comment)
             response += "\n{} + ({}) = {}".format(rolls, int(modifier), total)
         except:
             response = "指令錯誤：\n使用範例：roll 3d6+10 測試骰"
+        await ctx.send(response)
+
+    @commands.command()
+    async def coc(self, ctx, *args):
+        """coc 技能判定
+
+        範例：
+        coc 50
+        coc 50 聆聽"""
+        try:
+            dc = int(args[0])
+            comment = " ".join(args[1:])
+            (total, rolls, modifier) = self._roll("1d100")
+            if total <= 2:
+                result = "大成功"
+            elif total >= 99:
+                result = "大失敗"
+            elif total <= dc:
+                result = "成功"
+            else:
+                result = "失敗"
+            response = "{}\n{} (目標值：{}，擲骰結果：{})".format(ctx.author.mention, result, dc, total)
+            if comment:
+                response += "\n{}".format(comment)
+        except:
+            response = "{}\n指令錯誤：\n使用範例：coc 50"
         await ctx.send(response)
 
     def _roll(self, roll_string):
